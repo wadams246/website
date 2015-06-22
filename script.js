76e3fea9a4a562f7f8da98962c1be8c325f89a80
wadams246 <wadams246@gmail.com>
2015-06-21 23:34:16 -0400

    new

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("foot01").innerHTML = 
+	"<p>&copy; " + new Date().getFullYear() + " Wadams' website</p>";
+
+
+document.getElementById("nav02").innerHTML = 
+	"<div id='navbar'><ul>" +
+	"<li><a href='index.html' class='index'>Home</a></li>" + 
+	"<li><a href='customers.html' class='customers'>Customers</a></li>" +
+	"<li><a href='about.html' class='about'>About</a></li>" +
+	"<li><a href='#' class='contact'>Contact</a></li>"
+	"</div></ul>";
+
+document.getElementById("nav01").innerHTML = 
+	"<ul id='menu'>" +
+	"<li><a href='index.html'>Home</a></li>" +
+	"<li><a href='customers.html'>Data</a></li>" +
+	"<li><a href='about.html'>About</a></li>" +
+	"</ul>";
+
