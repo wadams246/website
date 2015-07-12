8990395bc3a3ac70b1f30090fdde8ab0b6de7ba0
wadams246 <wadams246@gmail.com>
2015-07-12 02:30:05 -0400

    7-12-15

--- practice.js
+++ practice.js
@@ -1,11 +1,23 @@
-var me = {
-    firstName: "Bill",
-    lastName: "Adams",
-    address: {
-        street: "11 2nd St.",
-        city: "Dunbar",
-        state: "PA"
-    },
-    phone: "724-984-2627"
-};
+var arr1 = ["1", "2", "3", "4", "5"];
 
+function mapForEach(arr1, fn) {
+    
+    var total = 0;
+    for (var i = 0; i < arr1.length; i++) {
+        total += (
+            fn(arr1[i])
+        )
+    };
+    
+    return total;k
+}
+
+
+console.log(arr1);
+
+var arr2 = mapForEach(arr1, function(item) {
+    console.log(item);
+    return parseInt(item);
+});
+
+console.log(arr2);
