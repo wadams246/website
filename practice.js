8b1e726de125907d92a0a17ca7454a1e3437b6b9
wadams246 <wadams246@gmail.com>
2015-07-15 01:30:10 -0400

    7-15-15

--- practice.js
+++ practice.js
@@ -1,12 +1,21 @@
-function init() {
-    var stage = new createjs.State(id("demoCanvas"));
-    var circle = new createjs.Shape();
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
 
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    
-    stage.addChile(circle);
-    stage.update();
-}
 
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(240, 160, 100, 40);
+ctx.strokeStyle = "rdba(0, 0, 255, .1)";
+ctx.stroke();
+ctx.closePath();
