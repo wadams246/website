d92a15a3628d6da862d62a740ff1b229f9f479ac
wadams246 <wadams246@gmail.com>
2015-07-15 16:16:35 -0400

    7/15/15 4:16

--- practice.js
+++ practice.js
@@ -1,39 +1,34 @@
-<<<<<<< HEAD
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
 
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(240, 160, 100, 40);
-ctx.strokeStyle = "rdba(0, 0, 255, .1)";
-ctx.stroke();
-ctx.closePath();
-=======
-<<<<<<< HEAD
-=======
-function init() {
-    var stage = new createjs.State(id("demoCanvas"));
-    var circle = new createjs.Shape();
-
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    
-    stage.addChile(circle);
-    stage.update();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#009AFF";
+    ctx.fill();
+    ctx.closePath();
 }
 
->>>>>>> 1ff4fdaea4954c609ad0986651e69e071f91c485
->>>>>>> 85ac1cc0def7f80af412817703818b3564dc8808
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+}
+
+setInterval(draw, 10);
