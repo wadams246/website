307241e908bf620a7ec44eb8ee7394c573a9dc43
wadams246 <wadams246@gmail.com>
2015-07-18 16:11:11 -0400

    added collision detection

--- practice.js
+++ practice.js
@@ -26,7 +26,7 @@ var bricks = [];
 for (c = 0; c<brickColumnCount; c++) {
     bricks[c] = [];
     for(r = 0; r<brickRowCount; r++) {
-        bricks [c] [r] = { x: 0, y: 0 };
+        bricks [c] [r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -51,6 +51,20 @@ function keyUpHandler(e) {
     }
 }
 
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) { 
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -71,15 +85,17 @@ function drawBall() {
 function drawBricks() {
     for (c = 0; c < brickColumnCount; c++) {
         for (r = 0; r < brickRowCount; r++) {
-            var brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
-            var brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
-            bricks [c][r].x = brickX;
-            bricks [c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks[c][r].status ==1) {
+                var brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks [c][r].x = brickX;
+                bricks [c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -89,7 +105,7 @@ function draw() {
     drawBricks();
     drawBall();
     drawPaddle();
-    
+    collisionDetection();
     
     
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
